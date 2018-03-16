b3f66a161f81c6d82fbaecc366ccc2a968a36a2b
Michal Bujalski <bujalski.michal@gmail.com>
2018-03-16 22:11:41 +0100

    Moved views to separate file

--- src/Main.elm
+++ src/Main.elm
@@ -1,53 +1,22 @@
 module Main exposing (..)
 
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (program)
 import Models exposing (..)
 import Updates exposing (update)
 import Msgs exposing (Msg)
-import RemoteData exposing (..)
 import Commands exposing (..)
----- MODEL ----
+import Views exposing (view)
 
 
 init : ( Model, Cmd Msg )
 init =
     ( defaultModel, fetchCurrency currencyUrl)
 
----- VIEW ----
-
-
-view : Model -> Html Msg
-view model =
-    currencyFetchView model
-
-currencyFetchView : Model -> Html Msg
-currencyFetchView model =
-  case model.currency of
-      Loading ->
-          div [] [ text "Loading..." ]
-      NotAsked ->
-          div [] []
-      Failure failure ->
-          div [] [ text <| toString failure ]
-      Success payload ->
-          currencyView payload
-
-currencyView : Currency -> Html Msg
-currencyView currency =
-  div []
-    [ div [] [ text <| currency.base ++ " - " ++ currency.target ]
-    , div [] [ text <| "$ " ++ currency.price ]
-    ]
-
----- PROGRAM ----
-
-
 main : Program Never Model Msg
 main =
-    Html.program
-        { view = view
-        , init = init
-        , update = Updates.update
-        , subscriptions = always Sub.none
-        }
+    program
+      { view = view
+      , init = init
+      , update = Updates.update
+      , subscriptions = always Sub.none
+      }
---
+++ src/Views.elm
@@ -0,0 +1,30 @@
+module Views exposing (..)
+
+import Html exposing (Html, text, div, h1, img)
+import Html.Attributes exposing (src)
+import Models exposing (Model, Currency)
+import Msgs exposing (Msg)
+import RemoteData exposing (..)
+
+view : Model -> Html Msg
+view model =
+    currencyFetchView model
+
+currencyFetchView : Model -> Html Msg
+currencyFetchView model =
+  case model.currency of
+      Loading ->
+          div [] [ text "Loading..." ]
+      NotAsked ->
+          div [] []
+      Failure failure ->
+          div [] [ text <| toString failure ]
+      Success payload ->
+          currencyView payload
+
+currencyView : Currency -> Html Msg
+currencyView currency =
+  div []
+    [ div [] [ text <| currency.base ++ " - " ++ currency.target ]
+    , div [] [ text <| "$ " ++ currency.price ]
+    ]
